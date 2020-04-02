89cf362b1c434b2078b2615a177ad92a18e4eb10
kevlarr <echo1@dvnl.net>
2020-04-01 21:13:48 -0400

    sigh wip

--- src/middleware/load_user.rs
+++ src/middleware/load_user.rs
@@ -3,9 +3,8 @@ use crate::{extensions::Assigns, models::AuthorWithoutPassword, State};
 use actix_service::{Service, Transform};
 use actix_session::{Session, UserSession};
 use actix_web::{dev::{ServiceRequest, ServiceResponse}, web::{self, Data}, Error};
-use futures::future::{ok, Ready, FutureExt};
+use futures::future::{ok, Ready};
 use std::task::{Context, Poll};
-use std::future::Future;
 
 pub struct LoadUser;
 
@@ -34,12 +33,13 @@ pub struct LoadUserMiddleware<S> {
 impl<S, B> Service for LoadUserMiddleware<S>
 where
     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
-    S::Future: Future,
+    S::Future: 'static,
 {
     type Request = ServiceRequest;
     type Response = ServiceResponse<B>;
     type Error = Error;
     type Future = S::Future;
+
 
     fn poll_ready(&mut self, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
         self.service.poll_ready(cx)
@@ -49,54 +49,27 @@ where
         let auth = req.get_session().get::<i32>("auth");
         let data = req.app_data::<State>();
 
-        async {
-            self.service.call(req)
+        if let (Ok(Some(id)), Some(state)) = (auth, data) {
+            let pool = &mut *state.pool.borrow_mut();
+
+            //sqlx::query_as!(
+                //AuthorWithoutPassword,
+                //"select id, email, name from author where id = $1",
+                //id,
+            //).fetch_one(pool).await.map_err(|e| e.to_string())
+
+            //let mut exts = req.head().extensions_mut();
+            //let assigns = exts.get_mut::<Assigns>();
+
+            //println!("ID: {}", id);
+            //println!("AUTHOR: {:?}", result);
+            //println!("ASSIGNS: {:?}", assigns);
+
+            //if let (Some(Ok(author)), Some(assigns)) = (result, exts.get_mut::<Assigns>()) {
+                //assigns.user = Some(author);
+            //}
         }
 
-
-
-        //match (auth, data) {
-            //(Ok(Some(id)), Some(state)) => {
-                //let pool = &mut *state.pool.borrow_mut();
-
-                //sqlx::query_as!(
-                    //AuthorWithoutPassword,
-                    //"select id, email, name from author where id = $1",
-                    //id,
-                //).fetch_one(pool);
-
-                //self.service.call(req)
-            //},
-            //_ => self.service.call(req),
-        //}
-
-
-
-
-            //if let (Ok(Some(id)), Some(state)) = (
-                //req.get_session().get::<i32>("auth"),
-                //req.app_data::<State>()
-            //) {
-
-                //let result = sqlx::query_as!(
-                    //AuthorWithoutPassword,
-                    //"select id, email, name from author where id = $1",
-                    //id,
-                //).fetch_one(pool).await;
-
-                //let mut exts = req.head().extensions_mut();
-                //let assigns = exts.get_mut::<Assigns>();
-
-                //println!("ID: {}", id);
-                //println!("AUTHOR: {:?}", result);
-                //println!("ASSIGNS: {:?}", assigns);
-
-                //if let (Some(Ok(author)), Some(assigns)) = (result, exts.get_mut::<Assigns>()) {
-                    //assigns.user = Some(author);
-                //}
-            //}
-        //}
-
-        //self.service.call(req)
+        self.service.call(req)
     }
 }
