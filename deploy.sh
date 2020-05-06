4bea826a23cdcf3a6a55ca377c901f410cd185dc
kevlarr <echo1@dvnl.net>
2020-05-05 23:44:36 -0400

    Set up build script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/sh
+
+if [ -z "$TARGETDIR" ]
+then
+    echo Error: Must set TARGETDIR
+    exit 1
+fi
+
+set -e
+
+VERSION=$(cat Cargo.toml | grep version -m 1 | grep -oE '[0-9\.]+')
+TARGETDIR="$TARGETDIR/$VERSION"
+
+echo "Building busybees:$VERSION"
+(
+    set -x
+    cargo build
+)
+
+echo "\nCopying binary and assets"
+(
+    set -x
+    mkdir $TARGETDIR
+    cp target/debug/server $TARGETDIR/server-$VERSION
+    cp .env $TARGETDIR/
+    cp -r www $TARGETDIR
+)
+
+echo "\nStarting server"
+(
+    set -x
+    nohup $TARGETDIR/server-$VERSION &
+)
+sleep 1
+cat nohup.out
