f37c077e162bed8ea8f2dfa08c3a91fc56733f35
kevlarr <6306496+kevlarr@users.noreply.github.com>
2021-02-25 16:40:16 -0500

    Routes in server bin

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,13 +2,16 @@ use std::{env, io};
 
 use actix_session::CookieSession;
 use actix_web::{
-    middleware::Logger,
+    middleware::{DefaultHeaders, Logger},
     App, HttpServer,
 };
 
 use busybees::{
+    actions as ax,
+    guards::auth_guard,
     middleware,
-    routes,
+    routes::Routes,
+    ASSET_BASEPATH,
     State,
 };
 
@@ -26,7 +29,49 @@ async fn main() -> io::Result<()> {
             .http_only(true)
             .secure(true);
 
-        let routes_ = routes::from_state(&state);
+        let routes = Routes::new("/")
+            .map_files(&ASSET_BASEPATH, "www/assets", |files| files.wrap(
+                DefaultHeaders::new().header("Cache-Control", "max-age=31536000")
+            ))
+            .map_files("/public", "www/public", |files| files.wrap(
+                DefaultHeaders::new().header("Cache-Control", "max-age=31536000")
+            ))
+            .map_files("/uploads", &state.upload_path, |files| files.wrap(
+                DefaultHeaders::new().header("Cache-Control", "max-age=31536000")
+            ))
+            .get("", ax::home)
+            .get("/about", ax::about)
+            .get("/sandbox", ax::sandbox)
+            .scope("/admin", |admin| admin
+                .guard(auth_guard)
+                .scope("/posts", |posts| posts
+                    .get("/", ax::admin::posts::get)
+                    .get("/new", ax::admin::posts::new)
+                    .get("/edit/{key}", ax::admin::posts::edit)
+                    .get("/delete/{key}", ax::admin::posts::delete)
+                )
+                .get("/test", ax::about)
+            )
+            .scope("/api", |api| api
+                .guard(auth_guard)
+                .scope("/posts", |posts| posts
+                    .scope("/{key}", |post| post
+                        .patch("/", ax::api::posts::update)
+                        .get  ("/images", ax::api::posts::images::list)
+                        .post ("/images/new", ax::api::posts::images::upload)
+                        .patch("/published", ax::api::posts::update_published)
+                    )
+                )
+            )
+            .scope("/auth", |auth| auth
+                .get("", ax::auth::get)
+                .post("", ax::auth::post)
+                .get("/clear", ax::auth::delete)
+            )
+            .scope("/posts", |posts| posts
+                .get("/{key}/read/{slug}", ax::posts::get_post)
+            )
+            .default(ax::not_found);
 
         App::new()
             .data(state)
@@ -38,7 +83,7 @@ async fn main() -> io::Result<()> {
             .wrap(cookie_session)
             .wrap(Logger::default())
 
-            .service(routes_)
+            .service(routes.service)
     };
 
     HttpServer::new(app)
--- src/routes.rs
+++ src/routes.rs
@@ -10,33 +10,25 @@ use actix_web::{
         ServiceResponse,
     },
     guard::fn_guard,
-    middleware::DefaultHeaders,
     web,
     FromRequest,
     Responder,
     Scope,
 };
 
-use crate::{
-    actions as ax,
-    guards::auth_guard,
-    ASSET_BASEPATH,
-    State,
-};
 
-
-struct Routes {
-    service: Scope,
+pub struct Routes {
+    pub service: Scope,
 }
 
 impl Routes {
-    fn new() -> Self {
+    pub fn new(path: &str) -> Self {
         Self {
-            service: web::scope("/"),
+            service: web::scope(path),
         }
     }
 
-    fn default<F, T, R, U>(mut self, handler: F) -> Self
+    pub fn default<F, T, R, U>(mut self, handler: F) -> Self
     where
         F: Factory<T, R, U>,
         T: FromRequest + 'static,
@@ -47,14 +39,14 @@ impl Routes {
         self
     }
 
-    fn scope(mut self, path: &str, cb: impl Fn(Routes) -> Routes) -> Self {
+    pub fn scope(mut self, path: &str, cb: impl Fn(Routes) -> Routes) -> Self {
         let routes = Routes { service: web::scope(path) };
 
         self.service = self.service.service(cb(routes).service);
         self
     }
 
-    fn guard<F>(mut self, f: F) -> Self
+    pub fn guard<F>(mut self, f: F) -> Self
     where
         F: Fn(&RequestHead) -> bool + 'static,
     {
@@ -62,7 +54,7 @@ impl Routes {
         self
     }
 
-    fn get<F, T, R, U>(mut self, path: &str, handler: F) -> Self
+    pub fn get<F, T, R, U>(mut self, path: &str, handler: F) -> Self
     where
         F: Factory<T, R, U>,
         T: FromRequest + 'static,
@@ -73,7 +65,7 @@ impl Routes {
         self
     }
 
-    fn post<F, T, R, U>(mut self, path: &str, handler: F) -> Self
+    pub fn post<F, T, R, U>(mut self, path: &str, handler: F) -> Self
     where
         F: Factory<T, R, U>,
         T: FromRequest + 'static,
@@ -84,7 +76,7 @@ impl Routes {
         self
     }
 
-    fn patch<F, T, R, U>(mut self, path: &str, handler: F) -> Self
+    pub fn patch<F, T, R, U>(mut self, path: &str, handler: F) -> Self
     where
         F: Factory<T, R, U>,
         T: FromRequest + 'static,
@@ -95,7 +87,7 @@ impl Routes {
         self
     }
 
-    fn map_files<F>(
+    pub fn map_files<F>(
         mut self,
         urlpath: &str,
         dirpath: &str,
@@ -119,52 +111,4 @@ impl Routes {
         self.service = self.service.service(cb(scope));
         self
     }
-}
-
-
-pub fn from_state(s: &State) -> Scope {
-    Routes::new()
-        .map_files(&ASSET_BASEPATH, "www/assets", |files| files.wrap(
-            DefaultHeaders::new().header("Cache-Control", "max-age=31536000")
-        ))
-        .map_files("/public", "www/public", |files| files.wrap(
-            DefaultHeaders::new().header("Cache-Control", "max-age=31536000")
-        ))
-        .map_files("/uploads", &s.upload_path, |files| files.wrap(
-            DefaultHeaders::new().header("Cache-Control", "max-age=31536000")
-        ))
-        .get("", ax::home)
-        .get("/about", ax::about)
-        .get("/sandbox", ax::sandbox)
-        .scope("/admin", |admin| admin
-            .guard(auth_guard)
-            .scope("/posts", |posts| posts
-                .get("/", ax::admin::posts::get)
-                .get("/new", ax::admin::posts::new)
-                .get("/edit/{key}", ax::admin::posts::edit)
-                .get("/delete/{key}", ax::admin::posts::delete)
-            )
-            .get("/test", ax::about)
-        )
-        .scope("/api", |api| api
-            .guard(auth_guard)
-            .scope("/posts", |posts| posts
-                .scope("/{key}", |post| post
-                    .patch("/", ax::api::posts::update)
-                    .get  ("/images", ax::api::posts::images::list)
-                    .post ("/images/new", ax::api::posts::images::upload)
-                    .patch("/published", ax::api::posts::update_published)
-                )
-            )
-        )
-        .scope("/auth", |auth| auth
-            .get("", ax::auth::get)
-            .post("", ax::auth::post)
-            .get("/clear", ax::auth::delete)
-        )
-        .scope("/posts", |posts| posts
-            .get("/{key}/read/{slug}", ax::posts::get_post)
-        )
-        .default(ax::not_found)
-        .service
 }
